fe23ebe19c5b1670550dbd05f5c5226da4ea0bc6
GodwinEgo <godwinehile04@gmail.com>
2023-05-23 14:56:24 +0100

    url

--- logoscan/urls.py
+++ logoscan/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from reviews import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('review/', views.review_logo, name='review_logo'),
+    path('reviews/', views.review_list, name='review_list'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ReviewForm
 
-# Create your views here.
+
+def review_logo(request):
+    if request.method == 'POST':
+        form = ReviewForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('review_list')
+    else:
+        form = ReviewForm()
+    return render(request, 'reviews/review_logo.html', {'form': form})
+
+
+def review_list(request):
+    reviews = Review.objects.all()
+    return render(request, 'reviews/review_list.html', {'reviews': reviews})
