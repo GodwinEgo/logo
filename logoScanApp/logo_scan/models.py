adb918f7c27ec4a6e67c7806e476a62c601c1593
Godwin Ego <godwinehile04@gmail.com>
2023-05-27 13:32:00 +0100

    model

--- logoScanApp/logo_scan/models.py
+++ logoScanApp/logo_scan/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Logo(models.Model):
+    name = models.CharField(max_length=1000)
+    image = models.ImageField(upload_to='logos/')
+
+
+class Review(models.Model):
+    logo = models.ForeignKey(Logo, on_delete=models.CASCADE)
+    rating = models.PositiveIntegerField()
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
